6c7349644f93c1f6c44eb12172449a4c0cec8f0f
choijungp <37467592+choijungp@users.noreply.github.com>
2023-02-14 11:41:32 +0900

    Create boj11055.swift

---
+++ BoJ/boj11055.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let n = Int(readLine()!)!
+let arr = readLine()!.split(separator: " ").map({ Int($0)! })
+var dp = Array(repeating: 0, count: n)
+dp[0] = arr[0]
+
+for i in 1..<n {
+    dp[i] = arr[i]
+    for j in 0..<i {
+        if arr[j] < arr[i] {
+            dp[i] = max(dp[i], dp[j] + arr[i])
+        }
+    }
+}
+print(dp.max()!)
