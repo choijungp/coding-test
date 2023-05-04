b2f22c103985a9e0abad2a4a033c4076932e8098
choijungp <37467592+choijungp@users.noreply.github.com>
2023-05-04 23:25:54 +0900

    Create boj2702.swift

---
+++ BoJ/boj2702.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let t = Int(readLine()!)!
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 { return a }
+    return gcd(b, a%b)
+}
+
+for _ in 0..<t {
+    let input = readLine()!.split(separator: " ").map({ Int($0)! })
+    print(input[0] * input[1] / gcd(input[0], input[1]), gcd(input[0], input[1]))
+}
