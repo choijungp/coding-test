b4c2da3e962ffe6e7d9db320f5bbe94ca1d9132b
Jungin Choi <37467592+choijungp@users.noreply.github.com>
2024-03-10 04:03:05 +0900

    Create boj1946.swift

---
+++ BoJ/boj1946.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+let file = FileIO()
+
+let t = file.readInt()
+for _ in 0..<t {
+    let n = file.readInt()
+    var people: [(p: Int, i: Int)] = []
+    for i in 0..<n {
+        people.append((file.readInt(), file.readInt()))
+    }
+    people = people.sorted(by: { $0.0 < $1.0 })
+    
+    var cnt = 1, tmpI = people[0].i
+    for i in 1..<n {
+        if people[i].i < tmpI {
+            cnt += 1
+            tmpI = people[i].i
+        }
+    }
+    print(cnt)
+}
